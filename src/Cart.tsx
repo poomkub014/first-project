7c55e9350eac636b5665e47f516da9712bb065c3
kiattipoom <poomkub014@gmail.com>
2024-08-11 11:40:18 +0700

    add quantity

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -4,7 +4,7 @@ import { CartContext } from './useContext/useContext'
 
 const Cart = () => {
 
- const {cart,removeFromCart} = useContext(CartContext);
+ const {cart,removeFromCart,increaseQuantity,decreaseQuantity} = useContext(CartContext);
 
 
   return (
@@ -12,8 +12,10 @@ const Cart = () => {
       <h1>Shopping Cart</h1>
    
   {cart.map((item)=>(
-    <div key={item.id}>{item.title}
-   <div onClick={()=>removeFromCart(item.id)}>Remove</div>
+    <div key={item.id}>{item.title} - {item.quantity}
+     <button onClick={()=>increaseQuantity(item.id)}>+</button>
+     <button onClick={()=>decreaseQuantity(item.id)}>-</button>
+   <button onClick={()=>removeFromCart(item.id)}>Remove</button>
     </div>
 
   ))}
--- src/useContext/useContext.tsx
+++ src/useContext/useContext.tsx
@@ -6,13 +6,38 @@ export const CartProvider = ({children}) => {
     const [cart,setCart] =useState([])
   
     const addToCart = (product) =>{
-      setCart([...cart,product])
+      const existingProduct = cart.find(item => item.id === product.id)
+
+      if(existingProduct){
+        
+          setCart(cart.map(item => item.id === product.id
+            ?{...item,quantity:item.quantity+1}
+        :item
+          ));
+      }else{
+        setCart([...cart,{...product,quantity:1}]);
+      }
     };
 
-    const removeFromCart = (productId) =>{
-        setCart(cart.filter(item => item.id !== productId));
+    const removeFromCart = (product) =>{
+        setCart(cart.filter(item => item.id !== product.id));
       };
+
+    const increaseQuantity = (productId) =>{
+      setCart(cart.map(item => item.id === productId
+        ? {...item,quantity:item.quantity+1}
+        : item
+      ));
+    };
+
+    const decreaseQuantity = (productId) =>{
+      setCart(cart.map(item => item.id === productId && item.quantity > 1
+        ? {...item,quantity:item.quantity-1}
+        : item
+      ));
+    };
+
     return (
-        <CartContext.Provider value ={{cart,addToCart,removeFromCart}}>{children}</CartContext.Provider>
+        <CartContext.Provider value ={{cart,addToCart,removeFromCart,increaseQuantity,decreaseQuantity}}>{children}</CartContext.Provider>
     );
 };
