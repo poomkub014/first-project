d016c177934ef1d0c6ea098c525dd1216f56724b
PooM <157963540+poomkub014@users.noreply.github.com>
2024-08-14 08:32:06 +0000

    edit log in code

---
+++ src/useContext/user.tsx
@@ -0,0 +1,13 @@
+import React,{createContext,useState} from "react";
+
+ const UserContext = createContext(null);
+
+export const UserProvider = ({children}) =>{
+    const [user,setUser] = useState({username:'Guest'});
+    
+   return (
+       <UserContext.Provider value ={{user,setUser}}>{children}</UserContext.Provider>
+   );
+};
+
+export default UserContext
